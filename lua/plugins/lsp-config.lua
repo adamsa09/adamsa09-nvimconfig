009ec4144b0e0bffcbf2d2d8de2a4187f520c58a
Adam Sarhan <66484976+adamsa09@users.noreply.github.com>
2025-02-05 06:50:42 -0500

    Added alpha-nvim, modified telescope and lspconfig

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "pylsp" },
+				ensure_installed = { "pyright" },
       })
 		end,
 	},
